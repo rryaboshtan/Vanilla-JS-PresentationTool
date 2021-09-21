0256cf0c061c464a16253bc909a66590df958c7e
rryaboshtan <rrvworkroman@gmail.com>
2021-09-22 00:58:15 +0300

    change navigator class

--- assets/js/navigator.js
+++ assets/js/navigator.js
@@ -1,10 +1,51 @@
-import loadSlides from "./slideLoader.js";
+import loadSlides from './slideLoader.js';
+import Router from './router.js';
 
-export default class Navigator {
+export default class Navigator extends HTMLElement {
    constructor(slides, deck) {
+      super();
+      this.router = new Router();
+      this.route = this.router.getRoute();
+      this.slidesChangedEvent = new CustomEvent('slideschanged', {
+         bubbles: true,
+         cancelable: false,
+      });
+      this.router.eventSource.addEventListener('routechanged', () => {
+         if (this.route !== this.router.getRoute()) {
+            this.route = this.router.getRoute();
+            if (this.route) {
+               const slide = parseInt(this.route) - 1;
+               this.jumpTo(slide);
+            }
+         }
+      });
+
       this.deck = deck;
       this.slides = slides;
       this.jumpTo(0);
+   }
+
+   static get observedAttributes() {
+      return ['start'];
+   }
+
+   async attributeChangedCallback(attrName, oldVal, newVal) {
+      if (attrName === 'start') {
+         if (oldVal !== newVal) {
+            this.slides = await loadSlides(newVal);
+            this.route = this.router.getRoute();
+            let slide = 0;
+            if (this.route) {
+               slide = parseInt(this.route) - 1;
+            }
+            this.jumpTo(slide);
+         }
+      }
+   }
+   connectedCallback() {
+      setTimeout(() => {
+      this.style.backgroundColor = 'red';
+      }, 0)
    }
 
    get currentIndex() {
@@ -30,7 +71,11 @@ export default class Navigator {
    jumpTo(slideIdx) {
       if (slideIdx >= 0 && slideIdx < this.totalSlides) {
          this.currentIndex = slideIdx;
-         this.deck.innerHTML = this.currentSlide.html;
+         // this.deck.innerHTML = this.currentSlide.html;
+         this.innerHTML = this.currentSlide.html;
+         this.router.setRoute(slideIdx + 1);
+         this.route = this.router.getRoute();
+         this.dispatchEvent(this.slidesChangedEvent);
       }
    }
 
@@ -46,3 +91,6 @@ export default class Navigator {
       }
    }
 }
+
+export const registerDeck = () => customElements.define('slide-deck', Navigator, {extends: 'div'});
+// export const registerDeck = () => customElements.define('slide-deck', Navigator);
