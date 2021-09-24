24d1046ddc2cb14796298e37eab04fce3e77bd2d
rryaboshtan <rrvworkroman@gmail.com>
2021-09-25 02:55:10 +0300

    add animator class

--- assets/js/animator.js
+++ assets/js/animator.js
@@ -1,3 +1,42 @@
 export default class Animator {
-    constructor ()
+   constructor() {
+      this._transitioning = false;
+      this._firstStepAnim = 'first-step-anim';
+      this.secondStepAnim = 'second-step-anim';
+   }
+
+   get transitioning() {
+      return this._transitioning;
+   }
+
+   // get animationDone() {
+   //     return !!
+   // }
+
+   stepByStepAnimation(htmlElemName, callback, stepAnimClass) {
+      this._transitioning = true;
+
+      const animationEnd = () => {
+         htmlElemName.removeEventListener('animationend', animationEnd);
+         htmlElemName.classList.remove(stepAnimClass);
+         this._transitioning = false;
+         callback();
+      };
+
+      htmlElemName.addEventListener('animationend', animationEnd, false);
+      htmlElemName.classList.add(stepAnimClass);
+   }
 }
+//    secondStepAnim(htmlElemName) {
+//       this._transitioning = true;
+
+//       const animationEnd = () => {
+//          htmlElemName.removeEventListener('animationend', animationEnd);
+//          htmlElemName.classList.remove(this._firstStepAnim);
+//          this._transitioning = false;
+//          callback();
+//       };
+
+//       htmlElemName.addEventListener('animationend', animationEnd, false);
+//       htmlElemName.classList.add(this._firstStepAnim);
+//    }
