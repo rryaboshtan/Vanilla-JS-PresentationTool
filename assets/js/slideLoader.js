fe0d48f1f5d970b7eee9049a170006bb0b09be32
rryaboshtan <rrvworkroman@gmail.com>
2021-09-26 22:56:39 +0300

    add jsDoc for the Slide class

--- assets/js/slideLoader.js
+++ assets/js/slideLoader.js
@@ -1,5 +1,10 @@
 import Slide from './slide.js';
 
+/**
+ * Load a single slide
+ * @param {string} slideName 
+ * @returns {Promise<Slide>} The slide
+ */
 async function loadSlide(slideName) {
    const response = await fetch(`./assets/slides/${slideName}.html`);
    const slide = await response.text();
@@ -8,6 +13,11 @@ async function loadSlide(slideName) {
    return new Slide(slide);
 }
 
+/**
+ * 
+ * @param {string} start The name of the first viewed slide
+ * @returns {Promise<Slide[]>} The array of loaded slides
+ */
 export default async function loadSlides(start) {
    let next = start;
    const slides = [];
