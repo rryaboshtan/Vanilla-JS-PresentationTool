6e1babd12722ba5eedcd18cc0ae14ad3ff97584e
rryaboshtan <rrvworkroman@gmail.com>
2021-09-26 22:37:19 +0300

    add jsDoc to the Slide class

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,17 +1,26 @@
 export default class Slide {
+   /**
+    *
+    * @param {string} text The content of the slide
+    */
    constructor(text) {
-      this.text = text;
+      /** @property {HTMLDivElement} _html Wrapper div element for the slide content  */
       this._html = document.createElement('div');
+
       this._html.innerHTML = text;
       this._html2 = document.createElement('div');
       this._html2.appendChild(this._html);
       this._html = this._html2;
+
+      /** @property {string} _title title of the slide  */
       this._title = this._html.querySelectorAll('title');
+
       if (this._title && this._title.length) {
          this._title = this._title[0].innerText;
       }
 
       console.log('this.title = ', this._title);
+
       const nextSlides = this._html.querySelectorAll('.nextslide');
       console.log('nextSlides.length = ', nextSlides.length);
       console.log('this._html = ', this._html);
@@ -23,11 +32,18 @@ export default class Slide {
          this._nextSlideName = null;
       }
    }
-
+   /**
+    * The slide title
+    * @returns {string} The slide title
+    */
    get title() {
       return this._title;
    }
 
+   /**
+    *  HTML content of the slide
+    * @returns {HTMLDivElement} html content of the slide
+    */
    get html() {
       return this._html.innerHTML;
    }
@@ -35,6 +51,9 @@ export default class Slide {
    //    return '<div>' + this._html.innerHTML + '</div>';
    // }
 
+   /**
+    * @returns {string} The name of the next slide (filename without the .html extension)
+    */
    get nextSlideName() {
       return this._nextSlideName;
    }
