2413974bbb17611f06b6c01b01fa175f040e0000
rryaboshtan <rrvworkroman@gmail.com>
2021-09-26 22:25:59 +0300

    add jsDoc for Router class

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,31 +1,56 @@
-export default class Router {
+/**
+ * Handles routing for the app
+ */
+class Router {
    constructor() {
+      /**
+       * @property {HTMLDivElement} _eventSource the div used to generate events
+       */
       this._eventSource = document.createElement('div');
-      this.routeChanged = new CustomEvent('routechanged', {
+
+      /**
+       * @property {CustomEvent} _routeChanged
+       */
+      this._routeChanged = new CustomEvent('routechanged', {
          bubbles: true,
          cancelable: false,
       });
 
+      /**
+       * @property {string} _urlHash see window.location.hash
+       */
       this._urlHash = null;
 
       window.addEventListener('popstate', () => {
          if (this.getUrlHash() !== this._urlHash) {
             this._urlHash = this.getUrlHash();
-            this.eventSource.dispatchEvent(this.routeChanged);
+            this.eventSource.dispatchEvent(this._routeChanged);
          }
       });
    }
 
+   /**
+    * @returns {HTMLDivElement}
+    */
    get eventSource() {
       return this._eventSource;
    }
 
+   /**
+    * Set the url hash number
+    * @param {string} _urlHash 
+    */
    setRoute(_urlHash) {
       window.location.hash = _urlHash;
       this._urlHash = _urlHash;
     }
     
+   /**
+    * @returns {string}
+    */
     getUrlHash() {
         return window.location.hash.substr(1);
     }
 }
+
+export default Router;
