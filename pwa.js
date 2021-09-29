ab15e92e35412aa2de726bde5c847602a39734ad
rryaboshtan <rrvworkroman@gmail.com>
2021-09-30 01:45:37 +0300

    Add service worker fetch event listener

--- pwa.js
+++ pwa.js
@@ -54,9 +54,42 @@ async function installServiceWorker() {
 }
 
 self.addEventListener('install', (event) => {
-   console.log('[SW]: install');
+   //    console.log('[SW]: install');
+   event.waitUntil(Promise.all([installServiceWorker(), self.skipWaiting()]));
 });
 
 self.addEventListener('activate', (event) => {
    console.log('[SW]: activate');
 });
+
+self.addEventListener('fetch', (event) => {
+   // console.log('Fetch', event.request.url);
+   const { request } = event;
+
+   const url = new URL(request.url);
+
+   if (url.origin === location.origin) {
+      event.respondWith(cacheFirst(request));
+   } else {
+      event.respondWith(networkFirst(request));
+   }
+   // event.respondWith(cacheFirst(event.request));
+});
+
+async function cacheFirst(request) {
+   const cached = await caches.match(request);
+   return cached ?? (await fetch(request));
+}
+
+async function networkFirst(request) {
+   const cache = await caches.open(dynamicCacheName);
+   try {
+      const responce = await fetch(request);
+      await cache.put(request, responce.clone());
+      return responce;
+   } catch (e) {
+      const cached = await cache.match(request);
+      return cached ?? (await caches.match('/offline.html'));
+   }
+}
+
